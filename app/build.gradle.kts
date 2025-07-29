b5b705f7dc4a569f0ab07e954ae6400d5287a96a
thunderbiscuit <thunderbiscuit@protonmail.com>
2025-07-29 15:47:05 -0400

    chore: update Android dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,34 +51,34 @@ android {
 
 dependencies {
     // Basic android dependencies
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.2.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.datastore:datastore:1.1.1")
-    implementation("com.google.protobuf:protobuf-javalite:3.18.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("androidx.datastore:datastore:1.1.7")
+    implementation("com.google.protobuf:protobuf-javalite:4.31.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     // Jetpack Compose
     // Adding the Bill of Materials synchronizes dependencies in the androidx.compose namespace
     // You can remove the library version in your dependency declarations
-    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
+    implementation(platform("androidx.compose:compose-bom:2025.07.00"))
     implementation("androidx.compose.animation:animation")
     implementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.activity:activity-compose")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-    implementation("androidx.navigation:navigation-compose:2.8.0")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.23.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
+    implementation("androidx.navigation:navigation-compose:2.9.2")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.36.0")
 
     // Icons
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("com.composables:icons-lucide:1.0.0")
+    implementation("com.composables:icons-lucide:1.1.0")
 
     // Toolbar
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
 
     // Bitcoin Development Kit
     implementation("org.bitcoindevkit:bdk-android:2.0.0")
