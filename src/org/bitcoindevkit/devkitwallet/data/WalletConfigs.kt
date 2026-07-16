be555aae125c9a0c0a585af8dd13832250a61352
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-07-16 16:15:39 -0400

    chore: rename LICENSE to LICENSE.txt

--- src/org/bitcoindevkit/devkitwallet/data/WalletConfigs.kt
+++ src/org/bitcoindevkit/devkitwallet/data/WalletConfigs.kt
@@ -1,6 +1,6 @@
 /*
  * Copyright 2021-2026 thunderbiscuit and contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE file.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE.txt file.
  */
 
 package org.bitcoindevkit.devkitwallet.data
